0b29504e1a4a29c0b2fb8e3a707d74de576988db
Victor Petrunin <petruninviktor9464@gmail.com>
2023-03-22 14:56:12 +0300

    first commit

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+const sorting = require('../../app')
+
+describe('Books names test suit', () => {
+  it('Books names should be sorted in ascending order', () => {
+    expect(
+      sorting.sortByName([
+        'Гарри Поттер',
+        'Властелин Колец',
+        'Волшебник изумрудного города'
+      ])
+    ).toEqual([
+      'Властелин Колец',
+      'Волшебник изумрудного города',
+      'Гарри Поттер'
+    ])
+  })
+})
+
+describe('Books names test suit', () => {
+  it('Books names do not need to be sorted', () => {
+    expect(
+      sorting.sortByName([
+        'Гарри Поттер',
+        'Гарри Поттер',
+        'Гарри Поттер'
+      ])
+    ).toEqual(['Гарри Поттер', 'Гарри Поттер', 'Гарри Поттер'])
+  })
+})
